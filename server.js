19e0775cafa7965a3ebbadaa030353f79217f7ff
Sridevi Voleti <10664@in.techwave.net>
2018-09-15 15:10:42 +0530

    anged from maintenance to running mode

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-app.use((req,res,next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname+ '/public'));
 
